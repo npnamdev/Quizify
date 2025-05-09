8eacde2b4b60733d15c5b97cfbf64d0055cd16be
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-05-09 07:45:05 +0700

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -25,12 +25,6 @@ export default function LoginPage() {
         }
     };
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            router.push("/");
-        }
-    }, [router, isAuthenticated]);
-
     return (
         <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
             <div className="w-full max-w-sm md:max-w-3xl">
