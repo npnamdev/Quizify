2f968627bc4867e79c803b171ca5ac9486842c9e
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-05-12 15:03:16 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import ImageCropUploader from "@/components/ui-custom/ImageCropUploader";
-// import NotificationList from "@/components/ui-custom/NotificationList";
 
 export default function Home() {
   return (
     <div>
       <ImageCropUploader />
-      {/* <NotificationList /> */}
     </div>
   );
 }
