823c5d3b779813fad29931876087a07649b2a8e2
NamDev <npnam031101@gmail.com>
2025-05-13 13:26:54 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,10 @@
-"use client";
-
-import ConfirmDialog from "@/components/common/ConfirmDialog";
 import ImageCropUploader from "@/components/ui-custom/ImageCropUploader";
-import { toast } from "sonner"
 
 export default function Home() {
-  const handleDelete = () => {
-    console.log("Đã xác nhận xóa!");
-    toast.success("Đã xác nhận xóa!");
-  };
 
   return (
     <div>
       <ImageCropUploader />
-      <ConfirmDialog
-        title="Bạn có chắc chắn muốn xóa?"
-        description="Thao tác này không thể hoàn tác. Tài khoản và dữ liệu sẽ bị xóa vĩnh viễn."
-        confirmText="Xác nhận xóa"
-        cancelText="Hủy"
-        onConfirm={handleDelete}
-      />
     </div>
   );
 }
