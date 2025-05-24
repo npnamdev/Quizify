d1cabce4e4425750bce472b145576c65bc471f45
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-05-24 13:57:21 -0400

    update

--- hooks/useNotification.ts
+++ hooks/useNotification.ts
@@ -37,7 +37,7 @@ export const useNotification = () => {
 
     const notificationSound = useRef(
         new Howl({
-            src: ['/bell2.lnk'],
+            src: ['/bell2.mp3'],
             volume: 0.5,
             html5: true,
         })
